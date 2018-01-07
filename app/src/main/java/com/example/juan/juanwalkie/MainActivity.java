2f93347258551d63e3736f0dea261e1eac9482ab
Juan Rodriguez <sjdonado@uninorte.edu.co>
2018-01-07 16:25:24 -0500

    App working

---
+++ app/src/main/java/com/example/juan/juanwalkie/MainActivity.java
@@ -0,0 +1,120 @@
+package com.example.juan.juanwalkie;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_PERMISSIONS_REQUEST_RECORD_AUDIO = 200;
+    private static final int RC_SIGN_IN = 234;
+    private static boolean global_permissions = false;
+    public static GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    protected void onStart() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        super.onStart();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        requestPermissions();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.sign_in_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(global_permissions){
+                    signIn();
+                }else{
+                    Snackbar.make(view, "Accept permissions to continue", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            Toast.makeText(this, "signInResult:failed code=" + e.getStatusCode(), Toast.LENGTH_LONG).show();
+            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void updateUI(GoogleSignInAccount account){
+        if(account != null){
+            Intent intent = new Intent(getBaseContext(), PublicRoom.class);
+            intent.putExtra("ID", account.getId());
+            intent.putExtra("NAME", account.getDisplayName());
+            startActivity(intent);
+        }
+    }
+
+    private void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            global_permissions = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.RECORD_AUDIO}, MY_PERMISSIONS_REQUEST_RECORD_AUDIO);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_RECORD_AUDIO: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions();
+                }
+                return;
+            }
+        }
+    }
+
+    public static void signOut() {
+        mGoogleSignInClient.signOut();
+    }
+
+}
---
+++ app/src/main/java/com/example/juan/juanwalkie/PublicRoom.java
@@ -0,0 +1,297 @@
+package com.example.juan.juanwalkie;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Handler;
+import android.os.Vibrator;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class PublicRoom extends AppCompatActivity{
+
+    private MediaPlayer mPlayer;
+    private MediaPlayer beep_sound;
+    private MediaPlayer start_recording;
+    private MediaPlayer error_sound;
+    private MediaRecorder mRecorder;
+    private String mFileName;
+    private TextView text_status;
+    private GradientDrawable color_status;
+    private NotificationManager notificationManager;
+    private Vibrator vibrator;
+    private boolean recording = false;
+
+    private Socket mSocket;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public_room);
+
+        try {
+            IO.Options opts = new IO.Options();
+            opts.query = "name=" + getIntent().getStringExtra("NAME");
+            mSocket = IO.socket("https://juanwalkie.herokuapp.com", opts);
+        } catch (URISyntaxException e) {
+            Log.w("ERROR CONNECT SOCKET", "onCreate: mSocket", e);
+        }
+
+        setTitle("Public room");
+
+        mSocket.connect();
+
+        beep_sound = MediaPlayer.create(this, R.raw.radio_beep);
+        start_recording = MediaPlayer.create(this, R.raw.start_recording);
+        error_sound = MediaPlayer.create(this, R.raw.error_sound);
+
+        mFileName = getCacheDir().getAbsolutePath();
+        mFileName += "/juanwalkie.3gp";
+
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        text_status = findViewById(R.id.text_status);
+        final TextView text_users = findViewById(R.id.text_users);
+        final TextView text_log = findViewById(R.id.text_log);
+
+        View circle_status = findViewById(R.id.circle_status);
+        color_status = (GradientDrawable)circle_status.getBackground();
+
+        View record_audio = findViewById(R.id.record_audio);
+        final GradientDrawable bgShape = (GradientDrawable)record_audio.getBackground();
+
+        record_audio.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        bgShape.setColor(getResources().getColor(R.color.colorRecord));
+                        vibrator.vibrate(50);
+                        startRecording();
+                        return true;
+                    case MotionEvent.ACTION_UP:
+                        stopRecording();
+                        sendAudio();
+                        bgShape.setColor(getResources().getColor(R.color.colorPrimary));
+                        vibrator.vibrate(50);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        findViewById(R.id.sign_out).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mSocket.disconnect();
+                returnMainActivity();
+            }
+        });
+
+        mSocket.on("USERS", new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject data = (JSONObject) args[0];
+                        String total_users;
+                        String new_user;
+                        try {
+                            total_users = data.getString("total_users");
+                            new_user = data.getString("new_user");
+                        } catch (JSONException e) {
+                            return;
+                        }
+                        Log.i("TOTAL_USERS", total_users + "");
+                        text_users.setText("Connected users: " + total_users);
+                        if(!new_user.equals("nil")) text_log.setText(new_user + " is joined");
+                        text_status.setText("Online");
+                        color_status.setColor(getResources().getColor(R.color.online));
+                    }
+                });
+            }
+        });
+
+        color_status.setColor(getResources().getColor(R.color.offline));
+
+        mSocket.on("NEW_AUDIO", new Emitter.Listener() {
+            @Override
+            public void call(final Object... args) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject data = (JSONObject) args[0];
+                        String user_name;
+                        String audioBase64;
+                        byte[] decodedBytes;
+                        try {
+                            audioBase64 = data.getString("audio");
+                            Log.i("NEW_AUDIO", audioBase64);
+                            user_name = data.getString("name");
+                            decodedBytes = Base64.decode(audioBase64, 0);
+                            try {
+                                writeToFile(decodedBytes, mFileName);
+                                text_log.setText(user_name + " is talking");
+                                startPlaying(mFileName, text_log);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } catch (JSONException e) {
+                            Log.w("ERROR", "run: getAUDIO", e);
+                            return;
+                        }
+                    }
+                });
+            }
+        });
+
+        setNotification();
+    }
+
+    private void startRecording() {
+        start_recording.start();
+        mRecorder = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e("Media recoder", "prepare() failed");
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run(){
+                mRecorder.start();
+            }
+        }, 4 * 100);
+
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    private void startPlaying(String mFileName, final TextView text_log) {
+        beep_sound.start();
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(mFileName);
+            mPlayer.prepare();
+            mPlayer.setVolume(100, 100);
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run(){
+                    mPlayer.start();
+                }
+            }, 8 * 100);
+        } catch (IOException e) {
+            Log.e("PLAYING RECORD", "prepare() failed");
+        }
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                text_log.setText("Hold down to talk");
+            }
+        });
+    }
+
+    private void sendAudio (){
+        File file = new File(mFileName);
+        try {
+            byte[] FileBytes = FileUtils.readFileToByteArray(file);
+            byte[] encodedBytes = Base64.encode(FileBytes, 0);
+            String encodedString = new String(encodedBytes);
+            mSocket.emit("USER_AUDIO", encodedString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeToFile(byte[] data, String fileName) throws IOException{
+        FileOutputStream out = new FileOutputStream(fileName);
+        out.write(data);
+        out.close();
+    }
+
+    private void returnMainActivity(){
+        MainActivity.signOut();
+        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void setNotification(){
+        Intent resultIntent = new Intent(this, PublicRoom.class);
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0, resultIntent, 0);
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, "com.juan.status_notification")
+                        .setSmallIcon(R.drawable.ic_walkie_notification)
+                        .setContentTitle(getIntent().getStringExtra("NAME").split(" ")[0] + ", you're online")
+                        .setContentText("Tap to more options.")
+                        .setOngoing(true)
+                        .setContentIntent(resultPendingIntent);
+        notificationManager.notify(001, mBuilder.build());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        notificationManager.cancel(001);
+        mSocket.disconnect();
+    }
+
+    /*
+    private void isConnect(){
+        new Thread(new Runnable() {
+            public void run() {
+                ConnectivityManager cm = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+                if(isConnected){
+                    if(!mSocket.connected()){
+                        mSocket.connect();
+                        text_status.setText("online");
+                        color_status.setColor(getResources().getColor(R.color.online));
+                    }
+                }else{
+                    mSocket.disconnect();
+                    text_status.setText("offline");
+                    color_status.setColor(getResources().getColor(R.color.offline));
+                }
+            }
+        }).start();
+    }
+    */
+}
